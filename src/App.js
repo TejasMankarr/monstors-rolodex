5a5173990f34e6029c15374bf78d7c5bb92ed667
TejasMankarr <tejasmankar.coder16@gmail.com>
2021-08-21 14:44:09 +0530

    card-list component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      monsters:[]
+      monsters: []
     };
   }
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters:users}));
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-        }
+        <CardList monsters={this.state.monsters}></CardList>
+        
       </div>
     );
   }
